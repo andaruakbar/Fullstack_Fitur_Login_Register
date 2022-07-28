5e80304d0a1d85092483ae2dcf773c164fad0af5
andaru akbar <andaruakbar11@gmail.com>
2022-07-29 04:05:59 +0700

    backend

---
+++ Backend/index.js
@@ -0,0 +1,18 @@
+import express from "express";
+import dotenv from "dotenv";
+import db from "../Backend/config/Database.js";
+import router from "./routes/index.js";
+dotenv.config();
+const app = express();
+
+try {
+  await db.authenticate();
+  console.log("database conected...");
+} catch (error) {
+  console.log(error);
+}
+
+app.use(express.json());
+app.use(router);
+
+app.listen(5000, () => console.log("server running at port 5000"));
